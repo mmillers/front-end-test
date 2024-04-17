c3c7c3f1c08141cc420185923b215f469f3d55e2
Maximiller Santos <4600227+mmillers@users.noreply.github.com>
2024-04-17 15:25:19 -0300

    update server script to deploy to heroku

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-app.use(express.static(__dirname + "/dist/<app-name>"));
+app.use(express.static("./dist/teste-angular"));
 app.get("/*", (req, res) =>
-  res.sendFile("index.html", { root: "/dist/<app-name>" })
+  res.sendFile("index.html", { root: "dist/teste-angular/" })
 );
 app.listen(process.env.PORT || 8080);
